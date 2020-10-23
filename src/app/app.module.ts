9a489e5d30660cd4e10f2365c2ef76eabd8d76f9
Nattaphong Malaphan <natthaphong.ma.60@ubu.ac.th>
2020-10-24 03:37:12 +0700

    forTest01

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { RouterModule } from '@angular/router';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FriendsComponent } from './friends/friends.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FriendsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    RouterModule.forRoot([
+      {path: 'friends', component: FriendsComponent },
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
